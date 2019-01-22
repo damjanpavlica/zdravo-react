88df02ead95945e67db086fe59ae0287bb9b37a3
Damjan Pavlica <damjan.pavlica@code.edu.rs>
2019-01-22 19:54:38 +0100

    ucitani podaci

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,21 @@ import Utisak from './Utisak'
 import korisnici from '../data/korisnici.json'
 
 export default class Main extends Component {
+    state = {
+      randomUsers: []
+    }
+
+    componentDidMount() {
+      fetch('https://randomuser.me/api/?results=10')
+        .then(x => x.json())
+        .then(x => this.setState({
+          randomUsers: x.results
+        }))
+    }
+
     render() {
+        console.log(this.state.randomUsers);
+        // TODO: mapirati randomUsers za HTML i prikazati ih
 
         const korisniciJsx = korisnici
             .map((x, i) => <Utisak key={i} korisnik={x}/>)
